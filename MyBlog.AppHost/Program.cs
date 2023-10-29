d6667ce1c90ebbd24de1f56b3a21addb3847d2b8
David Fowler <davidfowl@gmail.com>
2023-10-28 21:33:18 -0700

    Enable enhanced navigation, try xff headers

--- MyBlog.AppHost/Program.cs
+++ MyBlog.AppHost/Program.cs
@@ -3,6 +3,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 var db = builder.AddPostgresContainer("pg").AddDatabase("db");
 
 builder.AddProject<Projects.MyBlog>("myblog")
-       .WithReference(db);
+       .WithReference(db)
+       .WithEnvironment("ASPNETCORE_FORWARDEDHEADERS_ENABLED", "true");
 
 builder.Build().Run();
