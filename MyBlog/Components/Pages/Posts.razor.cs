4083766c41683039e3096e5e52290433290c8598
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2023-10-31 16:58:04 +0000

    Alternative form post pattern that avoids dynamic @formname and deletes before reloading (#1)
    * Alternative form post pattern that avoids dynamic @formname and deletes before reloading
    
    * Use enhanced form and avoid PRG

--- MyBlog/Components/Pages/Posts.razor.cs
+++ MyBlog/Components/Pages/Posts.razor.cs
@@ -1,18 +1,21 @@
+using Microsoft.AspNetCore.Components;
+
 namespace MyBlog.Components.Pages;
 
 public partial class Posts
 {
     private Post[]? posts;
 
+    [SupplyParameterFromForm]
+    public int? PostToDelete { get; set; }
+
     protected override async Task OnInitializedAsync()
     {
+        if (PostToDelete.HasValue)
+        {
+            await Client.DeletePostAsync(PostToDelete.Value);
+        }
+
         posts = await Client.GetPostsAsync();
-    }
-
-    async Task DeletePost(int id)
-    {
-        await Client.DeletePostAsync(id);
-
-        Nav.NavigateTo("/posts");
     }
 }
