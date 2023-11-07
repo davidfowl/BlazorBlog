4c71cf7df5d81841e7e863667e83c38cf287619d
David Fowler <davidfowl@gmail.com>
2023-11-07 08:37:59 -0800

    Added kv and blobs

--- MyBlog.AppHost/Program.cs
+++ MyBlog.AppHost/Program.cs
@@ -1,19 +1,24 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddAzureProvisioning();
+
 var db = builder.AddPostgresContainer("pg").AddDatabase("db");
 
-// Azd doesn't currently support storage
-//var blobs = builder.AddAzureStorage("storage")
-//                   .UseEmulator()
-//                   .AddBlobs("blobs");
+var blobs = builder.AddAzureStorage("storage")
+                   .UseEmulator()
+                   .AddBlobs("blobs");
+
+var kv = builder.AddAzureKeyVault("vault");
 
 var api = builder.AddProject<Projects.MyBlog_Api>("myblogapi")
                  .WithReference(db)
+                 .WithReference(kv)
                  .WithLaunchProfile("https");
 
 builder.AddProject<Projects.MyBlog>("myblog")
        .WithReference(api)
-       // .WithReference(blobs)
+       .WithReference(kv)
+       .WithReference(blobs)
        .WithEnvironment("ASPNETCORE_FORWARDEDHEADERS_ENABLED", "true")
        .WithLaunchProfile("https");
 
